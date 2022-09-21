8cc065f0603d1bb04393823f17af3f21fc88d4ac
Yubraj Upadhaya <gyubraj104@gmail.com>
2022-09-21 10:18:25 +0545

    Fix code format using black package

--- complex_search/models.py
+++ complex_search/models.py
@@ -7,18 +7,19 @@ from django.core.validators import MinValueValidator
 
 class Student(models.Model):
 
-    name = models.CharField(max_length=255, verbose_name= "Student Name")
+    name = models.CharField(max_length=255, verbose_name="Student Name")
     dob = models.DateField(verbose_name="Date of Birth")
-    no_of_degree = models.IntegerField(verbose_name="How many Degree you have?", validators= [MinValueValidator(0, message="You can't provide negative value.")])
+    no_of_degree = models.IntegerField(
+        verbose_name="How many Degree you have?",
+        validators=[MinValueValidator(0, message="You can't provide negative value.")],
+    )
 
-    created_date = models.DateField(auto_now_add= True)
-
+    created_date = models.DateField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
         if self.dob > datetime.date.today():
             raise ValidationError("Your DOB can't be in future")
         super(Student, self).save(*args, **kwargs)
-
 
     def __str__(self) -> str:
         return self.name
